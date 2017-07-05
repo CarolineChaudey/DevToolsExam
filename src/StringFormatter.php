55c023322f29e585401cbd774a9dc887cd140c33
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-07-05 11:03:45 +0200

    untested toCamelCase

--- src/StringFormatter.php
+++ src/StringFormatter.php
@@ -5,6 +5,12 @@
     public function concat(String $before, String $after) {
       return $before . $after;
     }
+
+    public function toCamelCase(String $before, String $after) {
+      $before = strtolower($before);
+      $after = ucfirst(strtolower($after));
+      return concat($before, $after);
+    }
   }
 
 ?>
